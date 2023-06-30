6e5459e496a6bae985c5d0fc21751a343e41c97f
SergeiMukha <sergeimushonok@example.com>
2023-06-30 13:37:34 +0300

    helpfile removed

--- helpfile.js
+++
@@ -1,7 +0,0 @@
-const re = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/
-
-const date = new Date();
-
-console.log(date.toISOString())
-
-console.log(re.exec(${date.toISOString()}`)[0]);
